5c056e8da022906c24c7bb7a84af9f47d3e43248
monkeyduck174 <clementchen0709@gmail.com>
2025-04-18 15:56:10 +0800

    implementing TLS v1

--- FoodOrdering/src/foodordering/MyRegistry.java
+++ FoodOrdering/src/foodordering/MyRegistry.java
@@ -5,10 +5,21 @@
  */
 package foodordering;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.sql.SQLException;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 /**
  *
@@ -16,10 +27,11 @@ import java.sql.SQLException;
  */
 public class MyRegistry {
 
-    public static void main(String[] args) throws RemoteException, SQLException {
+    public static void main(String[] args) throws RemoteException, SQLException, FileNotFoundException, IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException, UnrecoverableKeyException, KeyManagementException {
+
         //initiate DB Connection
         DBConnection myConn = DBConnection.getInstance();
-        
+
         //DCOMS Code
         Registry reg = LocateRegistry.createRegistry(1099);
         reg.rebind("OrderService", new OrderServiceImplementation(myConn));
