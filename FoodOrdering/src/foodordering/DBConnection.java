9ca14267adf5f9aded0c67f0c009dc1bbd1ae6fc
jd1219 <141599457+jd1219@users.noreply.github.com>
2025-04-25 02:33:09 +0800

    remove unused imports

--- FoodOrdering/src/foodordering/DBConnection.java
+++ FoodOrdering/src/foodordering/DBConnection.java
@@ -11,11 +11,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
